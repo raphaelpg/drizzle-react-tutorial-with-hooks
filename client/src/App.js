fa873b443367cc27fb57f41bf7c6004ccdb7a348
raphaelpg <raphael_pinto@outlook.com>
2021-05-19 17:23:16 +0100

    made app.js a functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,41 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import ReadString from "./ReadString";
 import SetString from "./SetString";
 
-class App extends Component {
-  state = { loading: true, drizzleState: null };
+const App = (props) => {
+  const { drizzle } = props;
+  const [drizzleState, setDrizzleState] = useState(null)
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    const { drizzle } = this.props;
-
-    // subscribe to changes in the store
-    this.unsubscribe = drizzle.store.subscribe(() => {
-
-      // every time the store updates, grab the state from drizzle
-      const drizzleState = drizzle.store.getState();
-
-      // check to see if it's ready, if so, update local component state
-      if (drizzleState.drizzleStatus.initialized) {
-        this.setState({ loading: false, drizzleState });
+  useEffect(
+    () => {
+      const unsubscribe = drizzle.store.subscribe( () => {
+        const drizzleState = drizzle.store.getState()
+        if (drizzleState.drizzleStatus.initialized) {
+          setDrizzleState(drizzleState)
+          setLoading(false)
+        }
+      })
+      return () => {
+        unsubscribe()
       }
-    });
-  }
+    }, [drizzle.store, drizzleState, loading]
+  )
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    if (this.state.loading) return "Loading Drizzle...";
-    return (
+  return (
+    loading ? "Loading Drizzle..."
+    :
       <div className="App">
         <ReadString
-          drizzle={this.props.drizzle}
-          drizzleState={this.state.drizzleState}
+          drizzle={props.drizzle}
+          drizzleState={drizzleState}
         />
         <SetString
-          drizzle={this.props.drizzle}
-          drizzleState={this.state.drizzleState}
+          drizzle={props.drizzle}
+          drizzleState={drizzleState}
         />
       </div>
     );
-  }
 }
 
 export default App;
